bad6bf32af953afd29de49ff19320b7ad475b307
joaojorge <joaojorgedosanjos@gmail.com>
2024-08-21 17:23:54 -0300

    :tada: Commit inicial

---
+++ README.md
@@ -0,0 +1,5 @@
+# PensePython2e
+
+## Capítulo 10: Listas
+
+https://penseallen.github.io/PensePython2e/10-listas.html
---
+++ atividades/exemplo_loop_lista.py
@@ -0,0 +1,14 @@
+numbers = [42,5]
+even = []
+for i in range(len(numbers)):
+    numbers[i];
+    #print(numbers)
+    #print(numbers[i])
+    #print(i)
+    if numbers[i] % 2 == 0:
+        even.append(numbers[i])
+        
+print(even)
+        
+        
+    
